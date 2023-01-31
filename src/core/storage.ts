a20af315b503a01023ba5e49e374c098922ee703
egorobets <egorobets@atlassian.com>
2023-01-31 16:35:39 -0500

    Updated storage module to be able to save the color mode and fraction mode

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,26 +1,30 @@
-'An abstraction on top of the AsyncStorage module'
+"An abstraction on top of the AsyncStorage module";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const saveData = async (key: string, value: object) => {
+export const saveData = async (key: string, value: object | string) => {
   try {
-    const jsonValue = JSON.stringify(value);
-    await AsyncStorage.setItem(key, jsonValue);
+    const stringValue =
+      typeof value == "string" ? value : JSON.stringify(value);
+    await AsyncStorage.setItem(key, stringValue);
   } catch (e) {
     console.log(`Failed to save ${key} with error ${e}`);
   }
-}
-
+};
 
 export const fetchData = async (key: string) => {
   try {
-    const value: string|null = await AsyncStorage.getItem(key);
-    return value != null ? JSON.parse(value) : null;
-  } catch(e) {
+    const value: string | null = await AsyncStorage.getItem(key);
+    return value == null || typeof value == "string"
+      ? value
+      : JSON.parse(value);
+  } catch (e) {
     console.log(`Failed to fetch ${key} with error ${e}`);
   }
-}
+};
 
 export enum StorageKeys {
-  RECIPES = 'recipe-book'
+  RECIPES = "recipe-book",
+  COLOR = "color-mode",
+  FRACTION = "fraction-mode",
 }
