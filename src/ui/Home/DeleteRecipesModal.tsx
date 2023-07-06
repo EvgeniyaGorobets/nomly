056d8f5211a619e14025552bfab380af3a8dfe65
egorobets <egorobets@atlassian.com>
2023-07-06 18:46:40 -0400

    Swapped out NativeBase modal for Paper modal in DeleteRecipesModal"

--- src/ui/Home/DeleteRecipesModal.tsx
+++ src/ui/Home/DeleteRecipesModal.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
-import { Text, Row, Button, Modal, Heading, Column } from "native-base";
+import { View } from "react-native";
+import { Text, Portal, Modal, Button, Divider } from "react-native-paper";
 
 import { AppContext, AppContextType } from "../../AppContext";
 
@@ -12,48 +13,35 @@ export const DeleteRecipesModal = ({ isOpen, closeModal }: ModalProps) => {
   const context: AppContextType = useContext(AppContext);
 
   return (
-    <Modal isOpen={isOpen} size="lg">
-      <Modal.Content>
-        <Modal.Header>
-          <Row justifyContent="center" w="100%">
-            <Heading size="md">Delete all recipes?</Heading>
-          </Row>
-        </Modal.Header>
-        <Modal.Body>
-          <Text>
+    <Portal>
+      <Modal visible={isOpen}>
+        <View>
+          <Text variant="headlineLarge">Delete all recipes?</Text>
+        </View>
+        <Divider />
+        <View>
+          <Text variant="bodyLarge">
             This will permanently delete all recipes you currently have saved on
-            <Text fontWeight="bold"> nomly</Text>. Are you sure you want to
-            continue?
+            <Text style={{ fontWeight: "bold" }}> nomly</Text>. Are you sure you
+            want to continue?
           </Text>
-        </Modal.Body>
-        <Modal.Footer>
-          <Column w="100%">
-            <Button
-              _text={{ fontWeight: "medium" }}
-              marginBottom="10px"
-              onPress={() => {
-                context.saveRecipes({});
-                closeModal();
-              }}
-            >
-              Yes, delete all recipes
-            </Button>
-            <Button
-              onPress={() => closeModal()}
-              _light={{ borderColor: "error.500" }}
-              _dark={{ borderColor: "error.400" }}
-              _text={{
-                fontWeight: "medium",
-                _light: { color: "error.500" },
-                _dark: { color: "error.400" },
-              }}
-              variant="outline"
-            >
-              Cancel! Don't delete anything!
-            </Button>
-          </Column>
-        </Modal.Footer>
-      </Modal.Content>
-    </Modal>
+        </View>
+        <Divider />
+        <View>
+          <Button
+            mode="contained"
+            onPress={() => {
+              context.saveRecipes({});
+              closeModal();
+            }}
+          >
+            Yes, delete all recipes
+          </Button>
+          <Button mode="outlined" onPress={() => closeModal()}>
+            Cancel! Don't delete anything!
+          </Button>
+        </View>
+      </Modal>
+    </Portal>
   );
 };
