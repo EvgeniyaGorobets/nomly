8e94f62eb8c7ae7af85f1e885a7a9cc2577e2841
egorobets <egorobets@atlassian.com>
2023-07-07 16:06:09 -0400

    Reaplced NativeBase Alerts with Paper Banners

--- src/ui/Home/AlertList.tsx
+++ src/ui/Home/AlertList.tsx
@@ -1,14 +1,6 @@
 import React, { useContext } from "react";
-import {
-  Row,
-  Icon,
-  Heading,
-  Alert,
-  Text,
-  Column,
-  IconButton,
-} from "native-base";
-import { AntDesign } from "@expo/vector-icons";
+import { View } from "react-native";
+import { Banner, Text } from "react-native-paper";
 
 import { AppContext, AppContextType } from "../../AppContext";
 import { AppAlert, deleteAlert } from "../../core/alert";
@@ -17,55 +9,22 @@ export const AlertList = ({}) => {
   const context: AppContextType = useContext(AppContext);
 
   return (
-    <Column zIndex={100} w="100%">
+    <View>
       {context.alerts.map((alert: AppAlert, i: number) => (
-        <Alert
-          key={i}
-          status={alert.status}
-          variant="top-accent"
-          maxHeight="80px"
-          width="100%"
-          marginBottom="5px"
-          _dark={{ backgroundColor: "dark.400" }}
+        <Banner
+          visible={true}
+          actions={[
+            {
+              label: "Dismiss",
+              onPress: () => context.setAlerts(deleteAlert(i, context.alerts)),
+            },
+          ]}
+          icon="alert"
         >
-          <Row w="100%">
-            <Column>
-              <Alert.Icon />
-            </Column>
-            <Column
-              flexGrow={1}
-              flexShrink={1}
-              marginLeft="10px"
-              marginRight="-5px"
-            >
-              <Row>
-                <Heading size="xs" flexShrink={1} flexGrow={1}>
-                  {alert.title}
-                </Heading>
-                <IconButton
-                  height="25px"
-                  width="25px"
-                  icon={
-                    <Icon
-                      as={AntDesign}
-                      name="close"
-                      size="sm"
-                      _light={{ color: "dark.300" }}
-                      _dark={{ color: "light.500" }}
-                    />
-                  }
-                  onPress={() =>
-                    context.setAlerts(deleteAlert(i, context.alerts))
-                  }
-                />
-              </Row>
-              <Row>
-                <Text fontSize="xs">{alert.description}</Text>
-              </Row>
-            </Column>
-          </Row>
-        </Alert>
+          <Text variant="displayMedium">{alert.title}</Text>
+          <Text variant="bodyMedium">{alert.description}</Text>
+        </Banner>
       ))}
-    </Column>
+    </View>
   );
 };
