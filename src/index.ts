05f16b2c600b0cc96ad41cd7c699da56d88e46ed
egorobets <egorobets@atlassian.com>
2023-07-07 16:27:57 -0400

    Updated dependencies to be more compatible with expo. Note: doesn't work on web because of the reanimated library and the BottomSheet just seems to throw errors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ export {
   DarkModeTheme,
 } from "./ui";
 export { Stack } from "./Stack";
-export { AppContext, Preferences, defaultPreferences } from "./AppContext";
+export { AppContext, defaultPreferences } from "./AppContext";
 
+export type { Preferences } from "./AppContext";
 export type { RecipeBook } from "./core/recipe";
 export type { AppAlert } from "./core/alert";
