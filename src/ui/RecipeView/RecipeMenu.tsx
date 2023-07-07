dcb68be042f77dd044fb870838c2e430dc7486c8
egorobets <egorobets@atlassian.com>
2023-07-07 05:28:50 -0400

    Replaced RecipeMenu with paper Menu component

--- src/ui/RecipeView/RecipeMenu.tsx
+++ src/ui/RecipeView/RecipeMenu.tsx
@@ -1,6 +1,5 @@
-import React, { useContext } from "react";
-import { IconButton, Icon, Menu, Text, Row } from "native-base";
-import { AntDesign, Ionicons } from "@expo/vector-icons";
+import React, { useContext, useState } from "react";
+import { Menu, IconButton } from "react-native-paper";
 
 import { AppContext, AppContextType } from "../../AppContext";
 import { deleteRecipe, cloneRecipe } from "../../core/recipe";
@@ -8,61 +7,32 @@ import { deleteRecipe, cloneRecipe } from "../../core/recipe";
 export const RecipeMenu = ({ recipeName }: { recipeName: string }) => {
   const context: AppContextType = useContext(AppContext);
 
+  const [isVisible, setVisibility] = useState<boolean>(false);
+  const toggleVisiblity = () => {
+    setVisibility(!isVisible);
+  };
+
   return (
     <Menu
-      w="100%"
-      placement="bottom right"
-      defaultIsOpen={false}
-      _dark={{ backgroundColor: "dark.500" }}
-      _light={{ backgroundColor: "light.50" }}
-      trigger={(triggerProps) => (
-        <IconButton
-          {...triggerProps}
-          padding="5px"
-          icon={
-            <Icon
-              as={Ionicons}
-              name="ellipsis-vertical"
-              size="lg"
-              _dark={{ color: "light.400" }}
-              _light={{ color: "light.50" }}
-            />
-          }
-        />
-      )}
+      visible={isVisible}
+      onDismiss={toggleVisiblity}
+      anchor={<IconButton icon="dots-vertical" onPress={toggleVisiblity} />}
+      anchorPosition="bottom"
     >
       <Menu.Item
+        title="Clone recipe"
+        leadingIcon="content-copy"
         onPress={() =>
           context.saveRecipes(cloneRecipe(context.recipes, recipeName))
         }
-      >
-        <Row alignItems="center">
-          <Icon as={AntDesign} size="6" name="copy1" />
-          <Text paddingX="5px">Clone recipe</Text>
-        </Row>
-      </Menu.Item>
+      />
       <Menu.Item
+        title="Delete recipe"
+        leadingIcon="delete-outline"
         onPress={() =>
           context.saveRecipes(deleteRecipe(context.recipes, recipeName))
         }
-      >
-        <Row alignItems="center">
-          <Icon
-            as={AntDesign}
-            size="6"
-            name="delete"
-            _dark={{ color: "error.400" }}
-            _light={{ color: "error.500" }}
-          />
-          <Text
-            paddingX="5px"
-            _dark={{ color: "error.400" }}
-            _light={{ color: "error.500" }}
-          >
-            Delete recipe
-          </Text>
-        </Row>
-      </Menu.Item>
+      />
     </Menu>
   );
 };
