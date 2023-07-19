373ff6c3e647744bb8faecf840bf192521e1b9e1
egorobets <egorobets@atlassian.com>
2023-07-19 15:52:34 -0400

    Added docstrings and unit tests for recipe-book module

--- src/core/recipe-book.ts
+++ src/core/recipe-book.ts
@@ -13,6 +13,13 @@ export type RecipeBook = { [key: string]: Recipe };
 
 /* ---- FUNCTIONS --- */
 
+/**
+ * Updates the given recipe book with a new or updated recipe
+ * @param recipeBook The recipe book in which the recipe is stored
+ * @param newRecipe The new version of the recipe (or the new recipe)
+ * @param recipeName The name of the recipe
+ * @returns A copy of the recipe book, with the new or updated recipe
+ */
 export const updateRecipe = (
   recipeBook: RecipeBook,
   newRecipe: Recipe,
@@ -21,6 +28,12 @@ export const updateRecipe = (
   return { ...recipeBook, [recipeName]: newRecipe };
 };
 
+/**
+ * Deletes the recipe with recipeName from recipeBook
+ * @param recipeBook The recipe book containing the recipe
+ * @param recipeName The name of the recipe to delete
+ * @returns A copy of the recipe book, without the deleted recipe
+ */
 export const deleteRecipe = (
   recipeBook: RecipeBook,
   recipeName: string
@@ -36,14 +49,33 @@ export const deleteRecipe = (
   return clonedRecipeBook;
 };
 
+/**
+ * Creates a copy of the recipe called recipeName in the recipe book
+ * @param recipeBook The recipe book containing the recipe to be cloned
+ * @param recipeName The name of the recipe to clone
+ * @returns A copy of the recipe book, with another copy of the recipe with
+ * recipeName, but named "{recipeName} (Copy)" to keep key uniqueness
+ */
 export const cloneRecipe = (
   recipeBook: RecipeBook,
   recipeName: string
 ): RecipeBook => {
+  const recipe = recipeBook[recipeName];
+
+  // deep copy
+  const clonedYield: Yield = { ...recipe.yield };
+  const clonedIngredients: Ingredient[] = recipe.ingredients.map(
+    (ingredient) => {
+      return { ...ingredient };
+    }
+  );
+
   return {
     ...recipeBook,
     [`${recipeName} (Copy)`]: {
-      ...recipeBook[recipeName],
+      yield: clonedYield,
+      ingredients: clonedIngredients,
+      notes: recipe.notes,
     },
   };
 };
---
+++ tests/unit-tests/recipe-book.test.ts
@@ -0,0 +1,155 @@
+import type { Recipe, RecipeBook } from "../../src/core/recipe-book";
+import {
+  cloneRecipe,
+  deleteRecipe,
+  updateRecipe,
+} from "../../src/core/recipe-book";
+
+describe("cloneRecipe", () => {
+  it("returns a copy of the recipe book with the cloned recipe", () => {
+    const originalRecipe: Recipe = {
+      yield: { amount: 4, units: "servings" },
+      ingredients: [
+        { name: "broccoli", amount: 16, units: "oz" },
+        { name: "quinoa", amount: 1, units: "cups" },
+      ],
+      notes: "Mix broccoli and quinoa in a pan and bake.",
+    };
+    const recipeBook: RecipeBook = {
+      "Quinoa Broccoli Casserole": originalRecipe,
+    };
+
+    const newRecipeBook = cloneRecipe(recipeBook, "Quinoa Broccoli Casserole");
+
+    // the old recipe book should be unchanged
+    expect(Object.keys(recipeBook)).toStrictEqual([
+      "Quinoa Broccoli Casserole",
+    ]);
+
+    // the new recipe book should have the cloned recipe
+    expect(Object.keys(newRecipeBook).sort()).toStrictEqual([
+      "Quinoa Broccoli Casserole",
+      "Quinoa Broccoli Casserole (Copy)",
+    ]);
+    expect(newRecipeBook["Quinoa Broccoli Casserole (Copy)"]).toStrictEqual(
+      originalRecipe
+    );
+
+    // the new recipe should be a new object, NOT a reference to the same object
+    newRecipeBook["Quinoa Broccoli Casserole (Copy)"].yield.amount = 5;
+    expect(originalRecipe.yield.amount).toBe(4);
+    newRecipeBook["Quinoa Broccoli Casserole (Copy)"].ingredients[0].units =
+      "cups";
+    expect(originalRecipe.ingredients[0].units).toBe("oz");
+  });
+});
+
+describe("deleteRecipe", () => {
+  it("returns a copy of the recipe book without the deleted recipe", () => {
+    const recipeBook: RecipeBook = {
+      "Quinoa Broccoli Casserole": {
+        yield: { amount: 4, units: "servings" },
+        ingredients: [
+          { name: "broccoli", amount: 16, units: "oz" },
+          { name: "quinoa", amount: 1, units: "cups" },
+        ],
+        notes: "Mix broccoli and quinoa in a pan and bake.",
+      },
+      "Chocolate Chip Cookies": {
+        yield: { amount: 12, units: "cookies" },
+        ingredients: [
+          { name: "flour", amount: 2, units: "cups" },
+          { name: "sugar", amount: 16, units: "tbsp" },
+          { name: "chocolate chips", amount: 8, units: "oz" },
+          { name: "butter", amount: 100, units: "g" },
+        ],
+        notes: "Mix dry and wet ingredients. Roll into balls and bake.",
+      },
+    };
+
+    const newRecipeBook = deleteRecipe(recipeBook, "Chocolate Chip Cookies");
+
+    // the old recipe book should be unchanged
+    expect(Object.keys(recipeBook).sort()).toStrictEqual([
+      "Chocolate Chip Cookies",
+      "Quinoa Broccoli Casserole",
+    ]);
+
+    // the new recipe book shouldn't have the deleted recipe
+    expect(Object.keys(newRecipeBook)).toStrictEqual([
+      "Quinoa Broccoli Casserole",
+    ]);
+  });
+
+  // not sure I actually want it to do this
+  it("throws an error if you try to delete a recipe that doesn't exist", () => {
+    const recipeBook: RecipeBook = {};
+    expect(() => deleteRecipe(recipeBook, "nonexistent recipe")).toThrow();
+  });
+});
+
+describe("updateRecipe", () => {
+  it("returns a copy of the recipe book with the updated recipe", () => {
+    const originalRecipe: Recipe = {
+      yield: { amount: 4, units: "servings" },
+      ingredients: [
+        { name: "broccoli", amount: 16, units: "oz" },
+        { name: "quinoa", amount: 1, units: "cups" },
+      ],
+      notes: "Mix broccoli and quinoa in a pan and bake.",
+    };
+    const recipeBook: RecipeBook = {
+      "Quinoa Broccoli Casserole": originalRecipe,
+    };
+
+    const updatedRecipe: Recipe = {
+      yield: { amount: 3, units: "servings" },
+      ingredients: [
+        { name: "broccoli", amount: 400, units: "g" },
+        { name: "quinoa", amount: 0.5, units: "cups" },
+      ],
+      notes: "blah blah blah",
+    };
+    const newRecipeBook = updateRecipe(
+      recipeBook,
+      updatedRecipe,
+      "Quinoa Broccoli Casserole"
+    );
+
+    // the old recipe book should be unchanged
+    expect(recipeBook["Quinoa Broccoli Casserole"]).toStrictEqual(
+      originalRecipe
+    );
+
+    // the new recipe book should have the new recipe
+    expect(newRecipeBook["Quinoa Broccoli Casserole"]).toStrictEqual(
+      updatedRecipe
+    );
+  });
+
+  it("returns a copy of the recipe book with the new recipe", () => {
+    const recipeBook: RecipeBook = {};
+    const newRecipe: Recipe = {
+      yield: { amount: 4, units: "servings" },
+      ingredients: [
+        { name: "broccoli", amount: 16, units: "oz" },
+        { name: "quinoa", amount: 1, units: "cups" },
+      ],
+      notes: "Mix broccoli and quinoa in a pan and bake.",
+    };
+
+    const newRecipeBook = updateRecipe(
+      recipeBook,
+      newRecipe,
+      "Quinoa Broccoli Casserole"
+    );
+
+    // the old recipe book should be unchanged
+    expect(recipeBook).toStrictEqual({});
+
+    // the new recipe book should have the new recipe
+    expect(newRecipeBook).toStrictEqual({
+      "Quinoa Broccoli Casserole": newRecipe,
+    });
+  });
+});
