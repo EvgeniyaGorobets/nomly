aa757ce8ae3aff622afbab8d76b7dc397681089c
egorobets <egorobets@atlassian.com>
2023-07-20 10:47:22 -0400

    Added more integration tests to AdjustableYield

--- tests/integration-tests/RecipeView/AdjustableYield.test.tsx
+++ tests/integration-tests/RecipeView/AdjustableYield.test.tsx
@@ -7,11 +7,11 @@ import { PaperProvider } from "react-native-paper";
 import { AdjustableYield } from "../../../src/ui/RecipeView/AdjustableYield";
 
 describe("AdjustableYield", () => {
+  const mockUpdateIngredients = jest.fn();
+
   beforeEach(() => {
     // needed for animations built into paper components
     jest.useFakeTimers();
-
-    const mockUpdateIngredients = jest.fn();
 
     render(
       <PaperProvider>
@@ -23,18 +23,28 @@ describe("AdjustableYield", () => {
     );
   });
 
-  it("changes the input value when you press x1, x2, or x4", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("changes the input value when you press x1, x2, or x4 and calls updateIngredients", () => {
     fireEvent.press(screen.getByText("x2"));
     expect(screen.queryByDisplayValue("12")).toBeNull();
     screen.getByDisplayValue("24");
+    expect(mockUpdateIngredients).toBeCalledTimes(1);
+    expect(mockUpdateIngredients).toHaveBeenLastCalledWith(24);
 
     fireEvent.press(screen.getByText("x4"));
     expect(screen.queryByDisplayValue("24")).toBeNull();
     screen.getByDisplayValue("48");
+    expect(mockUpdateIngredients).toBeCalledTimes(2);
+    expect(mockUpdateIngredients).toHaveBeenLastCalledWith(48);
 
     fireEvent.press(screen.getByText("x1"));
     expect(screen.queryByDisplayValue("48")).toBeNull();
     screen.getByDisplayValue("12");
+    expect(mockUpdateIngredients).toBeCalledTimes(3);
+    expect(mockUpdateIngredients).toHaveBeenLastCalledWith(12);
   });
 
   it("doesn't let you edit the yield manually when x1, x2, or x4 is selected", () => {
@@ -48,13 +58,16 @@ describe("AdjustableYield", () => {
     expect(screen.getByDisplayValue("48")).toBeDisabled();
   });
 
-  it("lets you edit the yield manually when 'custom' is selected", () => {
+  it("lets you edit the yield manually when 'custom' is selected and calls updateIngredients", () => {
     fireEvent.press(screen.getByText("Custom"));
     expect(screen.getByDisplayValue("12")).toBeEnabled();
 
     fireEvent.changeText(screen.getByDisplayValue("12"), "36");
     expect(screen.queryByDisplayValue("12")).toBe(null);
     screen.getByDisplayValue("36");
+
+    expect(mockUpdateIngredients).toBeCalledTimes(1);
+    expect(mockUpdateIngredients).toHaveBeenLastCalledWith(36);
   });
 
   it("resets the yield when you switch from 'custom' to x1, x2, or x4", () => {
@@ -89,19 +102,52 @@ describe("AdjustableYield", () => {
     screen.getByDisplayValue("48");
   });
 
-  it("shows an error when the yield is not a number", () => {
+  it("shows an error when the yield is not a number and doesn't call udpateIngredient", () => {
     expect(screen.queryByText("Recipe yield must be a number")).toBeNull();
 
     fireEvent.press(screen.getByText("Custom"));
-    fireEvent.changeText(screen.getByDisplayValue("12"), "abc"); // not a number!
+    fireEvent.changeText(screen.getByDisplayValue("12"), "abc");
 
-    // Not working for some reason, but works on my phone
     expect(screen.getByText("Recipe yield must be a number")).toBeVisible();
+    expect(mockUpdateIngredients).toBeCalledTimes(0);
+  });
+
+  it("shows an error when the yield is negative and doesn't call updateIngredient", () => {
+    const expectedErrorMsg = "Recipe yield must be greater than zero";
+    expect(screen.queryByText(expectedErrorMsg)).toBeNull();
+
+    fireEvent.press(screen.getByText("Custom"));
+    fireEvent.changeText(screen.getByDisplayValue("12"), "-2");
+
+    expect(screen.getByText(expectedErrorMsg)).toBeVisible();
+    expect(mockUpdateIngredients).toBeCalledTimes(0);
+  });
+
+  it("shows an error when the yield is zero and doesn't call updateIngredient", () => {
+    const expectedErrorMsg = "Recipe yield must be greater than zero";
+    expect(screen.queryByText(expectedErrorMsg)).toBeNull();
+
+    fireEvent.press(screen.getByText("Custom"));
+    fireEvent.changeText(screen.getByDisplayValue("12"), "0");
+
+    expect(screen.getByText(expectedErrorMsg)).toBeVisible();
+    expect(mockUpdateIngredients).toBeCalledTimes(0);
+  });
+
+  it("shows an error when the yield is en empty string and doesn't call updateIngredient", () => {
+    const expectedErrorMsg = "Recipe yield is required";
+    expect(screen.queryByText(expectedErrorMsg)).toBeNull();
+
+    fireEvent.press(screen.getByText("Custom"));
+    fireEvent.changeText(screen.getByDisplayValue("12"), "");
+
+    expect(screen.getByText(expectedErrorMsg)).toBeVisible();
+    expect(mockUpdateIngredients).toBeCalledTimes(0);
   });
 
   it("removes error messages when the yield is reset to one of the predefined amounts", () => {
     fireEvent.press(screen.getByText("Custom"));
-    fireEvent.changeText(screen.getByDisplayValue("12"), "abc"); // not a number!
+    fireEvent.changeText(screen.getByDisplayValue("12"), "abc");
     expect(screen.getByText("Recipe yield must be a number")).toBeVisible();
 
     fireEvent.press(screen.getByText("x4"));
