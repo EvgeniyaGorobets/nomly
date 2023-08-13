4f394fc542daf39a3c5bb25d345830ecf48bbd43
egorobets <egorobets@atlassian.com>
2023-08-13 17:32:32 -0400

    Fixed delete recipes modal styling by using Dialog instead of Modal component

--- src/ui/Home/DeleteRecipesModal.tsx
+++ src/ui/Home/DeleteRecipesModal.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
-import { View } from "react-native";
-import { Text, Portal, Modal, Button, Divider } from "react-native-paper";
+import { Button, Dialog, Portal, Text, useTheme } from "react-native-paper";
 
 import { AppContext, AppContextType } from "../../AppContext";
 
@@ -10,38 +9,41 @@ type ModalProps = {
 };
 
 export const DeleteRecipesModal = ({ isOpen, closeModal }: ModalProps) => {
+  const theme = useTheme();
   const context: AppContextType = useContext(AppContext);
 
   return (
     <Portal>
-      <Modal visible={isOpen}>
-        <View>
-          <Text variant="headlineLarge">Delete all recipes?</Text>
-        </View>
-        <Divider />
-        <View>
+      <Dialog visible={isOpen}>
+        <Dialog.Title>Delete all recipes?</Dialog.Title>
+        <Dialog.Content>
           <Text variant="bodyLarge">
             This will permanently delete all recipes you currently have saved on
             <Text style={{ fontWeight: "bold" }}> nomly</Text>. Are you sure you
             want to continue?
           </Text>
-        </View>
-        <Divider />
-        <View>
+        </Dialog.Content>
+        <Dialog.Actions style={{ justifyContent: "space-between" }}>
+          <Button
+            onPress={() => closeModal()}
+            style={{ padding: 5 }}
+            labelStyle={theme.fonts.bodyLarge}
+          >
+            Cancel
+          </Button>
           <Button
             mode="contained"
             onPress={() => {
               context.saveRecipes({});
               closeModal();
             }}
+            style={{ padding: 5 }}
+            labelStyle={theme.fonts.bodyLarge}
           >
             Yes, delete all recipes
           </Button>
-          <Button mode="outlined" onPress={() => closeModal()}>
-            Cancel! Don't delete anything!
-          </Button>
-        </View>
-      </Modal>
+        </Dialog.Actions>
+      </Dialog>
     </Portal>
   );
 };
