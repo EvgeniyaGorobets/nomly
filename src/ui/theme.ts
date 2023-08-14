c1a7333761431d3bfd0f4bbc18bef0f9b218422f
egorobets <egorobets@atlassian.com>
2023-08-14 19:28:56 -0400

    Swapped out color schemes for auto-generated ones created with https://callstack.github.io/react-native-paper/docs/guides/theming#creating-dynamic-theme-colors

--- src/ui/theme.ts
+++ src/ui/theme.ts
@@ -27,172 +27,95 @@ const fonts = {
 
 export const DefaultTheme = merge(CombinedLightTheme, {
   colors: {
-    primary: "#6cd4b2",
-    onPrimary: "#f9f9fa",
-    primaryContainer: "#6cd4b2",
-    onPrimaryContainer: "#f9f9fa",
-    secondaryContainer: "#89f5d2",
-    surfaceVariant: "#f9f9fa",
-    surface: "#edfcf6",
-    error: "#ef4444",
-    outline: "#bfbfbf",
+    primary: "rgb(0, 108, 83)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(129, 248, 209)",
+    onPrimaryContainer: "rgb(0, 33, 23)",
+    secondary: "rgb(76, 99, 90)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(206, 233, 220)",
+    onSecondaryContainer: "rgb(8, 32, 24)",
+    tertiary: "rgb(64, 99, 118)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(195, 232, 254)",
+    onTertiaryContainer: "rgb(0, 30, 43)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(251, 253, 249)",
+    onBackground: "rgb(25, 28, 27)",
+    surface: "rgb(251, 253, 249)",
+    onSurface: "rgb(25, 28, 27)",
+    surfaceVariant: "rgb(234, 240, 237)",
+    // ^ This is the only color I changed from the OG palette
+    // Originally it was "rgb(219, 229, 223)",
+    // But I moved the lightness from 88% to 93%
+    onSurfaceVariant: "rgb(63, 73, 68)",
+    outline: "rgb(112, 121, 116)",
+    outlineVariant: "rgb(191, 201, 195)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(46, 49, 47)",
+    inverseOnSurface: "rgb(239, 241, 238)",
+    inversePrimary: "rgb(99, 219, 181)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(238, 246, 241)",
+      level2: "rgb(231, 241, 236)",
+      level3: "rgb(223, 237, 231)",
+      level4: "rgb(221, 236, 229)",
+      level5: "rgb(216, 233, 226)",
+    },
+    surfaceDisabled: "rgba(25, 28, 27, 0.12)",
+    onSurfaceDisabled: "rgba(25, 28, 27, 0.38)",
+    backdrop: "rgba(41, 50, 46, 0.4)",
   },
   fonts: fonts,
 });
 
 export const DarkModeTheme = merge(CombinedDarkTheme, {
   colors: {
-    background: "#1f2022",
+    primary: "rgb(99, 219, 181)",
+    onPrimary: "rgb(0, 56, 42)",
+    primaryContainer: "rgb(0, 81, 62)",
+    onPrimaryContainer: "rgb(129, 248, 209)",
+    secondary: "rgb(178, 204, 192)",
+    onSecondary: "rgb(30, 53, 45)",
+    secondaryContainer: "rgb(52, 76, 67)",
+    onSecondaryContainer: "rgb(206, 233, 220)",
+    tertiary: "rgb(167, 204, 225)",
+    onTertiary: "rgb(12, 52, 69)",
+    tertiaryContainer: "rgb(39, 75, 93)",
+    onTertiaryContainer: "rgb(195, 232, 254)",
+    error: "rgb(255, 180, 171)",
+    onError: "rgb(105, 0, 5)",
+    errorContainer: "rgb(147, 0, 10)",
+    onErrorContainer: "rgb(255, 180, 171)",
+    background: "rgb(25, 28, 27)",
+    onBackground: "rgb(225, 227, 224)",
+    surface: "rgb(25, 28, 27)",
+    onSurface: "rgb(225, 227, 224)",
+    surfaceVariant: "rgb(63, 73, 68)",
+    onSurfaceVariant: "rgb(191, 201, 195)",
+    outline: "rgb(137, 147, 142)",
+    outlineVariant: "rgb(63, 73, 68)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(225, 227, 224)",
+    inverseOnSurface: "rgb(46, 49, 47)",
+    inversePrimary: "rgb(0, 108, 83)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(29, 38, 35)",
+      level2: "rgb(31, 43, 39)",
+      level3: "rgb(33, 49, 44)",
+      level4: "rgb(34, 51, 46)",
+      level5: "rgb(35, 55, 49)",
+    },
+    surfaceDisabled: "rgba(225, 227, 224, 0.12)",
+    onSurfaceDisabled: "rgba(225, 227, 224, 0.38)",
+    backdrop: "rgba(41, 50, 46, 0.4)",
   },
   fonts: fonts,
 });
-
-// extend the theme
-const NativeBaseTheme = {
-  colors: {
-    primary: {
-      50: "#c2f5e4",
-      100: "#a3edd4",
-      200: "#86e2c3",
-      300: "#6cd4b2",
-      400: "#4ecca3", // this is the logo colour
-      500: "#42be95",
-      600: "#43a785",
-      700: "#429076",
-      800: "#407a67",
-      900: "#3d6658",
-    },
-    dark: {
-      50: "#5e78b1",
-      100: "#526795",
-      200: "#4a5876",
-      300: "#3f4759",
-      400: "#2e3441",
-      500: "#272b31",
-      600: "#1f2022",
-      700: "#151515",
-      800: "#0a0908",
-      900: "#-3-3-2",
-    },
-    light: {
-      50: "#f9f9fa",
-      100: "#edf0f4",
-      200: "#d6dbe4",
-      300: "#c1c7d1",
-      400: "#abb2bf", // this is the 'light' color in OneDarker Palette
-      500: "#a2a7b0",
-      600: "#9c9d9f",
-      700: "#96938d",
-      800: "#928a7a",
-      900: "#8e8067",
-    },
-  },
-  fontSizes: {
-    "2xs": "10px",
-    xs: "12px",
-    sm: "16px",
-    md: "18px",
-    lg: "22px",
-    xl: "28px",
-    "2xl": "32px",
-    "3xl": "36px",
-  },
-  components: {
-    Input: {
-      defaultProps: {
-        variant: "underlined",
-        fontSize: "md",
-        _light: { color: "dark.600", borderColor: "light.200" },
-        _dark: {
-          color: "light.400",
-          borderColor: "light.500",
-          backgroundColor: "dark.600",
-        },
-        _focus: {
-          _dark: {
-            borderColor: "primary.300",
-            backgroundColor: "dark.500",
-          },
-        },
-        _disabled: {
-          borderBottomWidth: 0,
-          _dark: { color: "light.100" },
-        },
-      },
-    },
-    Text: {
-      defaultProps: {
-        fontSize: "md",
-        _light: { color: "dark.600" },
-        _dark: { color: "light.400" },
-      },
-    },
-    Heading: {
-      sizes: {
-        sm: {
-          fontSize: "18px",
-        },
-        md: {
-          fontSize: "22px",
-        },
-        lg: {
-          fontSize: "28px",
-        },
-      },
-      defaultProps: {
-        fontWeight: "medium",
-        _light: { color: "red.600" },
-        _dark: { color: "light.400" },
-      },
-    },
-    Icon: {
-      defaultProps: {
-        mx: "5px",
-        my: "5px",
-      },
-    },
-    // each screen is a Center component
-    Center: {
-      defaultProps: {
-        _dark: { bg: "dark.600" },
-        _light: { bg: "light.50" },
-        flex: 1,
-        px: 0,
-        height: "100%",
-        paddingBottom: "0px",
-      },
-    },
-    HStack: {
-      defaultProps: {
-        _light: { borderColor: "light.200" },
-        _dark: { borderColor: "light.500" },
-      },
-    },
-    VStack: {
-      defaultProps: {
-        _light: { borderColor: "light.200" },
-        _dark: { borderColor: "light.500" },
-      },
-    },
-    Button: {
-      defaultProps: {
-        _text: {
-          _dark: { color: "light.100" },
-          _light: { color: "light.50" },
-        },
-      },
-    },
-    FormControlErrorMessage: {
-      defaultProps: {
-        _text: {
-          _light: { color: "error.500" },
-          _dark: { color: "error.400" },
-        },
-      },
-    },
-  },
-  config: {
-    useSystemColorMode: false,
-    initialColorMode: "light",
-  },
-};
